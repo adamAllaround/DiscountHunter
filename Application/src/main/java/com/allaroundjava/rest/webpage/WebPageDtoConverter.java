94cd983a73f653ff816f0ab4b53d06bc8626c86a
Adam Stankowski <adam@allaroundjava.com>
2020-01-06 10:41:14 +0100

    Adding priceProposal field to webPageDto

--- Application/src/main/java/com/allaroundjava/rest/webpage/WebPageDtoConverter.java
+++ Application/src/main/java/com/allaroundjava/rest/webpage/WebPageDtoConverter.java
@@ -4,12 +4,15 @@ import com.allaroundjava.model.User;
 import com.allaroundjava.model.WebPage;
 import com.allaroundjava.rest.dto.WebPageDto;
 
+import java.math.BigDecimal;
+
 class WebPageDtoConverter {
 
     static WebPage fromDto(WebPageDto webPageDto, User user) {
         WebPage webPage = new WebPage();
         webPage.setUrl(webPageDto.getUrl());
         webPage.setUserOwner(user);
+        webPage.setPriceProposal(BigDecimal.valueOf(webPageDto.getPriceProposal()));
         return webPage;
     }
 
@@ -17,6 +20,7 @@ class WebPageDtoConverter {
         WebPageDto webPageDto = new WebPageDto();
         webPageDto.setUrl(webPage.getUrl());
         webPageDto.setId(webPage.getId());
+        webPageDto.setPriceProposal(webPage.getPriceProposal().doubleValue());
         return webPageDto;
     }
 }
